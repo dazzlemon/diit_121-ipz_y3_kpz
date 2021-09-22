6e1d617a52e64330302445679b9ca7390bdaf4bb
dazzlemon <daanill13470@gmail.com>
2021-09-22 20:17:07 +0300

    tensor3Val simplified

--- 2/src/main.pl
+++ 2/src/main.pl
@@ -11,30 +11,33 @@ correctTensor3Declaration(Name, X, Y, Z) :-
   is_of_type(positive_integer, Z).
 
 processIndex(I, Imax) :-
-  nonvar(I), verifyIndex(I, Imax).
+  nonvar(I) -> verifyIndex(I, Imax); between(1, Imax, I).
 
-tensor3Val_(Name, X, Y, Z, Val) :-
+tensor3Val_(Name, X, Y, Z) :-
   correctTensor3Declaration(Name, Xmax, Ymax, Zmax),
   processIndex(X, Xmax),
   processIndex(Y, Ymax),
   processIndex(Z, Zmax).
 
 tensor3Val(Name, X, Y, Z, Val) :-
-  tensor3Val_(Name, X, Y, Z, Val),
-  tensor3(Name, X, Y, Z, Val), !.
-tensor3Val(Name, X, Y, Z, empty) :-
-  tensor3Val_(Name, X, Y, Z, _).
+  tensor3Val_(Name, X, Y, Z),
+  (tensor3(Name, X, Y, Z, Val) -> true; Val=empty).
 
 tensor3IsCubic(tensor3_1) :-
   tensor3Declaration(tensor3_1, X, X, X).
+
+min(Name, V) :-
+  tensor3(Name, _, _, _, V), not((tensor3(Name, _, _, _, V1), V1 < V)).
+
+printTensor3(Name) :-
+  tensor3Declaration(Name, Xmax, Ymax, Zmax),
+  between(1, Xmax, X), between(1, Ymax, Y), between(1, Zmax, Z),
+  tensor3Val(Name, X, Y, Z, V), writeln(X:Z:Y/V), fail; true.
 
 printlist([]).
 printlist([X|List]) :-
     writeln(X),
     printlist(List).
-
-min(Name, V) :-
-  tensor3(Name, _, _, _, V), not((tensor3(Name, _, _, _, V1), V1 < V)).
 
 :-
   tensor3Val(tensor3_1, 1, 1, 1, Val), writeln('val at (1, 1, 1)' = Val),
@@ -43,10 +46,6 @@ min(Name, V) :-
 :-
   findall(X:Y:Z/V, tensor3Val(tensor3_1, X, Y, Z, V), Results), printlist(Results).
 
-printTensor3(Name) :-
-  tensor3Declaration(Name, Xmax, Ymax, Zmax),
-  between(1, Xmax, X), between(1, Ymax, Y), between(1, Zmax, Z),
-  tensor3Val(Name, X, Y, Z, V), writeln(X:Z:Y/V), fail; true.
 :- nl, printTensor3(tensor3_1).
 :- nl, min(tensor3_1, X), writeln(min=X).
   
