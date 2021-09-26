dd73211f64ad625f4643993659a87d11c09a24c1
dazzlemon <daanill13470@gmail.com>
2021-09-26 18:25:35 +0300

    upd for menu

--- 3/src/main.pl
+++ 3/src/main.pl
@@ -2,17 +2,43 @@ readchar(Ch) :-
     get_char(Ch),
     get_char(_).
 
+menuitem('0').
+menuitem('1') :-
+    writeln('--- INSERTING ---').
+    %assert.
+menuitem('2') :-
+    writeln('--- DELETING ---').
+    %retract
+menuitem('3') :-
+    consult('db.pl').
+menuitem('4') :-
+    %listing(),
+    write('press Enter'), get_char(_).
+menuitem('5') :-
+    writeln('--- COMMITTING CHANGES TO DATABASE'),
+    tell('db.pl'),
+    %listing(),
+    told.
+menuitem('6') :-
+    writeln('Choose something:'),% TODO
+    read(Num),
+    stud(Nm, Num, _, _),
+    writeln(Nm)
+    fail; true.
+menuitem('7') :-
+    %retractall(),
+    writeln('--- CLEARED ---').
 menuitem(X) :- writeln('You chose'=X).
 
 menu :-
     repeat, nl,
     writeln('1. INSERT'),
     writeln('2. DELETE'),
-    writeln('3. Load table'),
-    writeln('5. SELECT *'),
-    writeln('6. COMMIT'),
-    writeln('7. Save to file'),
-    writeln('8. Search'),
+    writeln('3. Load database'),
+    writeln('4. SELECT *'),
+    writeln('5. COMMIT'),
+    writeln('6. Search'),
+    writeln('7. Clear database'),
     writeln('0. Exit'),
     writeln('?>'), readchar(Num), menuitem(Num), Num='0'.
 :-menu.
