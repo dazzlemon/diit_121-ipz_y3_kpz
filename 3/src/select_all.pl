2dc00bf0a9f30308a9d2a82f50be6b255995973f
dazzlemon <daanill13470@gmail.com>
2021-09-28 19:58:24 +0300

    selectAll/4 fixed&done, need to remove unnecessary singleton vars

--- 3/src/select_all.pl
+++ 3/src/select_all.pl
@@ -11,29 +11,16 @@ selectAll :-
     write('(1 - university, 2 - faculty, 3 - department, 4 - lab): '),
     readchar(Nm),
     selectAll(Nm).
-selectAll('1') :-% list all universities
-    % writef(' %r\n', ['-', 108]),
-    % writeHeader(
-    %     ['ID', 'Name', 'President', 'VP'],
-    %     [   5,     20,          40,   40]),
-    % university(ID, Name, President, VP),
-    % writeSeparator([5, 20, 40, 40]),
-    % writef('|%5C|%20C|%40C|%40C|\n', [ID, Name, President, VP]),
-    % fail; writef(' %r\n', ['-', 108]).
-    selectAll(
-        university,
-        [ ID,   Name,   President,   VP],
-        ['ID', 'Name', 'President', 'VP'],
-        [   5,     20,          40,   40]).
-selectAll('2') :-% list all faculties
-    writef(' %r\n', ['-', 42]),
-    writeRow(
-        ['ID', 'UniversityID', 'Name'],
-        [   5,             15,     20]),
-    faculty(ID, UniversityID, Name),
-    writeSeparator([5, 15, 20]),
-    writef('|%5C|%15C|%20C|\n', [ID, UniversityID, Name]),
-    fail; writef(' %r\n', ['-', 42]).
+selectAll('1') :- selectAll(% list all universities
+    university,
+    [ ID,   Name,   President,   VP],
+    ['ID', 'Name', 'President', 'VP'],
+    [   5,     20,          40,   40]).
+selectAll('2') :- selectAll(% list all faculties
+    faculty,
+    [ ID,   UniversityID,   Name],
+    ['ID', 'UniversityID', 'Name'],
+    [   5,             15,     20]).
 selectAll('3') :-% list all departments
     true.% TODO
 selectAll('4') :-%list all labs
@@ -41,7 +28,6 @@ selectAll('4') :-%list all labs
 
 selectAll(Predicate, PredicateArgList, ColumnNameList, ColumnSizeList) :-
     writeStartEndSeparator(ColumnSizeList, TotalWidth),
-    writef(' %r\n', ['-', TotalWidth]),
     writeRow(ColumnNameList, ColumnSizeList),
     apply(Predicate, PredicateArgList),
     writeSeparator(ColumnSizeList),
