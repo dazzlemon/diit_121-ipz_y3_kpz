0e14d08f09da56a217602f6f7319dfcbbc00c41c
dazzlemon <daanill13470@gmail.com>
2021-09-30 17:00:19 +0300

    clear works as intended

--- 3/src/db.pl
+++ 3/src/db.pl
@@ -1,21 +1,21 @@
 :- dynamic university/4.
 :- multifile university/4.
 
-:- assertz(university(1, 'DNURT', 'Pshinko Alexander Nikolaevich', 'Bodnar Borys Yevhenovych')).
-:- assertz(university(2, test, test, test)).
+university(1, 'DNURT', 'Pshinko Alexander Nikolaevich', 'Bodnar Borys Yevhenovych').
+university(2, test, test, test).
 
 :- dynamic faculty/3.
 :- multifile faculty/3.
 
-:- assertz(faculty(1, 1, 'TK')).
+faculty(1, 1, 'TK').
 
 :- dynamic department/4.
 :- multifile department/4.
 
-:- assertz(department(1, 1, 1, 'KIT')).
+department(1, 1, 1, 'KIT').
 
 :- dynamic lab/2.
 :- multifile lab/2.
 
-:- assertz(lab(1, 1)).
+lab(1, 1).
 
