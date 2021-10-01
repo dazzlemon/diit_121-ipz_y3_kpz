577be72aa4cb66fa6b784eab5bba36b75843df18
dazzlemon <daanill13470@gmail.com>
2021-10-01 13:16:13 +0300

    both tasks done

--- 5/src/hello.erl
+++ 5/src/hello.erl
@@ -5,23 +5,27 @@ append([X|Xs], Ys) ->
 append([], Ys) ->
     Ys.
 
-filter_(P, L) ->
+filter(P, L) ->
   [ X || X <- L, P(X) ].
 
-countNumsAtomsTuples(Xs) ->
-  Nums   = filter_(is_number, Xs),
-  Atoms  = filter_(is_atom,   Xs),
-  Tuples = filter_(is_tuple,  Xs),
-  {length(Nums), length(Atoms), length(Tuples)}.
-
-% filter_(_, []) -> [];
-% filter_(P, [X|Xs]) ->
+% filter(_, []) -> [];
+% filter(P, [X|Xs]) ->
 %   case P(X) of
 %     true -> [X|filter_list(P, Xs)];
 %     false -> filter_list(P, Xs)
 %   end.
 
+countNumsAtomsTuples(Xs) ->
+  Nums   = filter(fun erlang:is_number/1, Xs),
+  Atoms  = filter(fun erlang:is_atom  /1, Xs),
+  Tuples = filter(fun erlang:is_tuple /1, Xs),
+  {length(Nums), length(Atoms), length(Tuples)}.
+
 main(_) ->
-  List = append([1, 2, 3], [4, 5]),
-  %NumAtomTupleCount = countNumsAtomsTuples([atom1, 1, {tuple, 1}, atom2, 3, 7]),
-  io:fwrite("~w~n", [List]).
+  {L1, L2} = {[1, 2, 3], [4, 5]},
+  List = append(L1, L2),
+  io:fwrite("append(~w, ~w) -> ~w~n", [L1, L2, List]),
+
+  List2 = [atom1, 1, {tuple, 1}, atom2, 3, 7],
+  {Nums, Atoms, Tuples} = countNumsAtomsTuples(List2),
+  io:fwrite("~w has ~w numbers, ~w atoms and ~w tuples~n", [List2, Nums, Atoms, Tuples]).
