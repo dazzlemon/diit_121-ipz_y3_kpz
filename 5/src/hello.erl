9f1e352b606a893382bc67d43507c9e895a5669f
dazzlemon <daanill13470@gmail.com>
2021-10-01 12:57:36 +0300

    append/2 done

--- 5/src/hello.erl
+++ 5/src/hello.erl
@@ -1,9 +1,27 @@
 -module(main_).
 
-concat(N, M) ->
-  lists:append(N, M).
+append([X|Xs], Ys) ->
+    [X|append(Xs, Ys)];
+append([], Ys) ->
+    Ys.
+
+filter_(P, L) ->
+  [ X || X <- L, P(X) ].
+
+countNumsAtomsTuples(Xs) ->
+  Nums   = filter_(is_number, Xs),
+  Atoms  = filter_(is_atom,   Xs),
+  Tuples = filter_(is_tuple,  Xs),
+  {length(Nums), length(Atoms), length(Tuples)}.
+
+% filter_(_, []) -> [];
+% filter_(P, [X|Xs]) ->
+%   case P(X) of
+%     true -> [X|filter_list(P, Xs)];
+%     false -> filter_list(P, Xs)
+%   end.
 
 main(_) ->
-  io:format("Hello, world!\n"),
-  List2 = concat([1, 2, 3], [4, 5]),
-  io:fwrite("~w~n", [List2]).
+  List = append([1, 2, 3], [4, 5]),
+  %NumAtomTupleCount = countNumsAtomsTuples([atom1, 1, {tuple, 1}, atom2, 3, 7]),
+  io:fwrite("~w~n", [List]).
