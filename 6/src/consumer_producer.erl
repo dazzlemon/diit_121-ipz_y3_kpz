0b98c0dfc765319bff9e21c1f7f84607774e793a
dazzlemon <daanill13470@gmail.com>
2021-10-02 22:12:45 +0300

    code for #6 done

--- 6/src/consumer_producer.erl
+++ 6/src/consumer_producer.erl
@@ -12,18 +12,18 @@
 buffer(Buf) -> 
 	io:format("Buffer = ~w~n", [Buf]), 
 	receive
-		%% реализовать по заданию структуру буфера
 		{get, Pid} when (length(Buf) > 0) -> 
 			[H|T] = Buf,
 			Pid!{item, H},
 			buffer(T);
-		{put, Value} -> buffer(Buf ++ Value);
+		{put, Value} -> buffer(Buf ++ [Value]);
 		{clear} -> buffer([]);
 		{stop} -> ok 
 	end.
 
 producer(NumberProc, CountIter) ->
-	[producer_iteration(NumberProc) || _ <- lists:seq(CountIter)].
+	[producer_iteration(NumberProc) || _ <- lists:seq(1, CountIter)],
+	io:format("Producer~w finished ~n", [NumberProc]).
 
 producer_iteration(NumberProc) ->
 	Elem = [NumberProc|produce(4)],
@@ -35,7 +35,7 @@ produce(X) ->
 
 consumer(NumberProc, 0) -> io:format("Consumer~w finished ~n",[NumberProc]);
 consumer(NumberProc, CountIter) ->
-	base!{get,self()},
+	base!{get, self()},
 	receive 
 		{item, Elem} -> 
 			io:format("Thread~w consumed ~w~n", [NumberProc, consume(Elem)]),
--- 6/src/main.erl
+++ 6/src/main.erl
@@ -1,20 +1,19 @@
 -module(main).
+-export([main/1]).
 -import(consumer_producer, [
     buffer/1,
 	producer/2,
 	consumer/2
 ]).
--compile(export_all).
 
-% main(_) ->
-start() ->
+main(_) ->
 	case whereis(base) of 
-		undefined -> register(base, spawn(erl2, bufer, [[]]));
+		undefined -> register(base, spawn(consumer_producer, buffer, [[]]));
 		_ -> base!{clear}
 	end,
 	io:format("Buffer initialized, all right!~n"),
 
-	[spawn(main, producer, [X, 5]) || X <- lists:seq(1, 5)],
+	[spawn(consumer_producer, producer, [X, 5]) || X <- lists:seq(1, 5)],
 
-	[spawn(main, consumer, [X, 5]) || X <- lists:seq(1, 5)],
-	io:format("Init finished....~n"). 
+	[spawn(consumer_producer, consumer, [X, 5]) || X <- lists:seq(1, 5)],
+	io:format("Init finished....~n").
