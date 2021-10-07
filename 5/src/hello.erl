7d3a05bbdf14d12abbf0b3e7e62fa179cd455149
dazzlemon <daanill13470@gmail.com>
2021-10-07 16:03:23 +0300

    rm filter, added proxy for countNumsAtomsTuples

--- 5/src/hello.erl
+++ 5/src/hello.erl
@@ -5,22 +5,13 @@ append([X|Xs], Ys) ->
 append([], Ys) ->
     Ys.
 
-filter(P, L) ->
-  [ X || X <- L, P(X) ].
-
-% filter(_, []) -> [];
-% filter(P, [X|Xs]) ->
-%   case P(X) of
-%     true -> [X|filter_list(P, Xs)];
-%     false -> filter_list(P, Xs)
-%   end.
-
-countNumsAtomsTuples([], R) -> R;
-countNumsAtomsTuples([H|T], {N, A, Tu}) ->
+countNumsAtomsTuples(Xs) -> countNumsAtomsTuples_(Xs, {0, 0, 0}).
+countNumsAtomsTuples_([], R) -> R;
+countNumsAtomsTuples_([H|T], {N, A, Tu}) ->
   if
-    is_number(H) -> countNumsAtomsTuples(T, {N+1, A, Tu});
-    is_atom(H) -> countNumsAtomsTuples(T, {N, A+1, Tu});
-    is_tuple(H) -> countNumsAtomsTuples(T, {N, A, Tu+1})
+    is_number(H) -> countNumsAtomsTuples_(T, {N + 1, A,     Tu});
+    is_atom(H)   -> countNumsAtomsTuples_(T, {N,     A + 1, Tu});
+    is_tuple(H)  -> countNumsAtomsTuples_(T, {N,     A,     Tu + 1})
   end.
 
 main(_) ->
@@ -29,5 +20,5 @@ main(_) ->
   io:fwrite("append(~w, ~w) -> ~w~n", [L1, L2, List]),
 
   List2 = [atom1, 1, {tuple, 1}, atom2, 3, 7],
-  {Nums, Atoms, Tuples} = countNumsAtomsTuples(List2, {0, 0, 0}),
+  {Nums, Atoms, Tuples} = countNumsAtomsTuples(List2),
   io:fwrite("~w has ~w numbers, ~w atoms and ~w tuples~n", [List2, Nums, Atoms, Tuples]).
