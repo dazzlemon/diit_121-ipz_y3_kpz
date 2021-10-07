3a761dc70f3bb803a0a567e481e5635d2164cce5
dazzlemon <daanill13470@gmail.com>
2021-10-07 16:00:51 +0300

    single loop countAtomsTuples

--- 5/src/hello.erl
+++ 5/src/hello.erl
@@ -15,11 +15,13 @@ filter(P, L) ->
 %     false -> filter_list(P, Xs)
 %   end.
 
-countNumsAtomsTuples(Xs) ->
-  Nums   = filter(fun erlang:is_number/1, Xs),
-  Atoms  = filter(fun erlang:is_atom  /1, Xs),
-  Tuples = filter(fun erlang:is_tuple /1, Xs),
-  {length(Nums), length(Atoms), length(Tuples)}.
+countNumsAtomsTuples([], R) -> R;
+countNumsAtomsTuples([H|T], {N, A, Tu}) ->
+  if
+    is_number(H) -> countNumsAtomsTuples(T, {N+1, A, Tu});
+    is_atom(H) -> countNumsAtomsTuples(T, {N, A+1, Tu});
+    is_tuple(H) -> countNumsAtomsTuples(T, {N, A, Tu+1})
+  end.
 
 main(_) ->
   {L1, L2} = {[1, 2, 3], [4, 5]},
@@ -27,5 +29,5 @@ main(_) ->
   io:fwrite("append(~w, ~w) -> ~w~n", [L1, L2, List]),
 
   List2 = [atom1, 1, {tuple, 1}, atom2, 3, 7],
-  {Nums, Atoms, Tuples} = countNumsAtomsTuples(List2),
+  {Nums, Atoms, Tuples} = countNumsAtomsTuples(List2, {0, 0, 0}),
   io:fwrite("~w has ~w numbers, ~w atoms and ~w tuples~n", [List2, Nums, Atoms, Tuples]).
